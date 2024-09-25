750a9e3858e04adbfae81f68088266ca03120538
stmcpeters <stmcpeters@gmail.com>
2024-09-25 12:01:42 -0700

    removes template info from components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNavBar from './components/Navbar'
-import ListStudents from './components/ListStudents'
-
+import Weather from './components/Weather';
 
 function App() {
 
   return (
     <div className="App">
       <MyNavBar />
-      <ListStudents />
-
+      <Weather />
     </div>
   )
 }
