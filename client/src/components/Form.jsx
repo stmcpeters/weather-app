7bc33435ba1b38d9adde4e65064c7511e950a1a8
stmcpeters <stmcpeters@gmail.com>
2024-09-26 00:09:38 -0700

    attempts to create dropdown menu to select users

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,33 +1,48 @@
-import { Button, Form } from 'react-bootstrap'
-// form to set favorite city
+import { Form } from 'react-bootstrap'
+import React, { useState } from 'react'
+import Dropdown from './Dropdown';
 
-// input
-
-// button to set favorite city
+  // button to set favorite city
 const SetFavoriteCity = () => {
+  const [selectedUser, setSelectedUser] = useState(null);
 
-  // handles what to do when submit button is clicked
-  const handleSubmit = (event) => {
+    // handles what to do when submit button is clicked
+    const handleSubmit = (event) => {
     // prevents default submission behaviors like refreshing/redirecting page
     event.preventDefault();
-    if(handleValidation()) {
+    if(selectedUser) {
     // runs fetchWeather() that gets data from API
       fetchWeather();
-     } else {
-      alert('Please enter a valid city!')
-     }
-  }
+      } else {
+      alert('Please select a user!')
+      }
+    }
+
+    // fetches weather data from backend using selected user's favorite city
+    const fetchWeather = async () => {
+      try {
+        // uses backend port to connect to API and uses inputted city name
+          const response = await fetch(`http://localhost:8080/weather?city=${selectedUser.favorite_city}`);
+        // waits for response and parses to json format
+          const data = await response.json();
+        // updates state of weather data using data from API
+          setWeatherData(data);
+        // catches and handles errors fetching data from API
+        } catch (error) {
+          console.error("Error fetching user's favorite city weather data: ", error);
+      }
+    }
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input>
+        <div>
+          <Dropdown onSelect={(value) => setSelectedUser(value)} />
+          {selectedUser && (
+            <Form onSubmit={handleSubmit}>
 
-        </input>  
-        <button>
-
-        </button>
-      </form>
+            </Form>
+          )}
+        </div>
     </div>
   )
 }
