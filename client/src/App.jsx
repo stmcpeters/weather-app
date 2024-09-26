cb300b00b1faa693de56de37e8ba944d84f49366
stmcpeters <stmcpeters@gmail.com>
2024-09-25 22:33:34 -0700

    renames component to form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,14 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNavBar from './components/Navbar'
 import Weather from './components/Weather';
-import SetFavoriteCity from './components/SetFavoriteCity';
+import Form from './components/Form';
 
 function App() {
 
   return (
     <div className="App">
       <MyNavBar />
-      <SetFavoriteCity />
+      <Form />
       <Weather />
     </div>
   )
