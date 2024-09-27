0ec450cf9343da19eea62f1b703d7457c9fbdb07
stmcpeters <stmcpeters@gmail.com>
2024-09-26 19:25:36 -0700

    adds message to user when creating new user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,25 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNavBar from './components/Navbar'
 import Weather from './components/Weather';
-import Form from './components/Form';
+import UserForm from './components/UserForm';
+import React, { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState({
+    username: "",
+    favorite_city: "",
+    email: ""
+  })
+
+  const onSaveUser = partialUser => {
+    setUser(partialUser);
+    alert(`Welcome! You're signed in as ${partialUser.username}`)
+  }
 
   return (
     <div className="App">
       <MyNavBar />
-      <Form />
+      <UserForm onSaveUser={onSaveUser} />
       <Weather />
     </div>
   )
