b1b6cbb9e3d1339d7eec6a27218f229047574995
stmcpeters <stmcpeters@gmail.com>
2024-10-01 12:55:58 -0700

    dumps database used for this project and saves in file

--- server/db.sql
+++ server/db.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 13.3
--- Dumped by pg_dump version 14.2
+-- Dumped from database version 14.13 (Homebrew)
+-- Dumped by pg_dump version 14.13 (Homebrew)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -21,22 +21,24 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- Name: students; Type: TABLE; Schema: public; Owner: -
+-- Name: users; Type: TABLE; Schema: public; Owner: beigeh0ney
 --
 
-CREATE TABLE public.students (
-    id integer NOT NULL,
-    firstname character varying(255),
-    lastname character varying(255),
-    is_current boolean
+CREATE TABLE public.users (
+    userid integer NOT NULL,
+    username character varying(255) NOT NULL,
+    favorite_city character varying(255),
+    email character varying(255)
 );
 
 
+ALTER TABLE public.users OWNER TO beigeh0ney;
+
 --
--- Name: students_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+-- Name: users_userid_seq; Type: SEQUENCE; Schema: public; Owner: beigeh0ney
 --
 
-CREATE SEQUENCE public.students_id_seq
+CREATE SEQUENCE public.users_userid_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -45,41 +47,54 @@ CREATE SEQUENCE public.students_id_seq
     CACHE 1;
 
 
---
--- Name: students_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.students_id_seq OWNED BY public.students.id;
-
+ALTER TABLE public.users_userid_seq OWNER TO beigeh0ney;
 
 --
--- Name: students id; Type: DEFAULT; Schema: public; Owner: -
+-- Name: users_userid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: beigeh0ney
 --
 
-ALTER TABLE ONLY public.students ALTER COLUMN id SET DEFAULT nextval('public.students_id_seq'::regclass);
+ALTER SEQUENCE public.users_userid_seq OWNED BY public.users.userid;
 
 
 --
--- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: -
+-- Name: users userid; Type: DEFAULT; Schema: public; Owner: beigeh0ney
 --
 
-COPY public.students (id, firstname, lastname, is_current) FROM stdin;
+ALTER TABLE ONLY public.users ALTER COLUMN userid SET DEFAULT nextval('public.users_userid_seq'::regclass);
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: beigeh0ney
+--
+
+COPY public.users (userid, username, favorite_city, email) FROM stdin;
+3       Maria   New York City   maria@gmail.com
+2       Kevin   Houston kevin@gmail.com
+1       Steph   San Francisco   steph@gmail.com
 \.
 
 
 --
--- Name: students_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+-- Name: users_userid_seq; Type: SEQUENCE SET; Schema: public; Owner: beigeh0ney
 --
 
-SELECT pg_catalog.setval('public.students_id_seq', 1, false);
+SELECT pg_catalog.setval('public.users_userid_seq', 20, true);
 
 
 --
--- Name: students students_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: beigeh0ney
 --
 
-ALTER TABLE ONLY public.students
-    ADD CONSTRAINT students_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (userid);
+
+
+--
+-- Name: users users_username_key; Type: CONSTRAINT; Schema: public; Owner: beigeh0ney
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_username_key UNIQUE (username);
 
 
 --
